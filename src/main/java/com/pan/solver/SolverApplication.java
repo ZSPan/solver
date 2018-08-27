17b8e0bdc629b8006d2f56cbf36bf13aa9f520eb
pan <545516891@qq.com>
2018-08-27 22:20:24 +0800

    init

---
+++ src/main/java/com/pan/solver/SolverApplication.java
@@ -0,0 +1,12 @@
+package com.pan.solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolverApplication.class, args);
+	}
+}
